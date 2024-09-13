3be17a84a7d8b201810098786b1b69d34a1af20f
Neeraj Kumawat <nkumawat34@gmail.com>
2024-09-13 10:18:19 +0530

    Added Order Style

--- client/src/components/orders.js
+++ client/src/components/orders.js
@@ -36,26 +36,27 @@ export default function Orders() {
   
     <div>
       {token?
-      <div className='flex flex-col'>
-        <h1 className='text-center text-4xl'>Orders</h1>
-        {products.length === 0 ? (
-          <p>No orders available</p>
-        ) : (
-          products.map((product, index) => (
-            <div key={index} className='flex flex-col justify-center items-center gap-4 mt-4 w-full'>
-              <h1 className='text-2xl'>Order {index + 1}</h1>
-              <img src="/images/order.jfif" alt={`Order ${index + 1}`} />
-              {product.map((item, idx) => (
-                <div key={idx} className='flex gap-4'>
-                  <div className='text-1xl'>{idx + 1}. {item.name}</div>
-                  <div className='text-1xl'>Price: {item.price} * Quantity: {item.quantity}</div>
-                  <div className='text-1xl'>Total: {item.price * item.quantity}</div>
-                </div>
-              ))}
-            </div>
-          ))
-        )}
-      </div>:<h1 className='text-3xl text-center mt-5'>Please Sign in</h1>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-center text-4xl mb-8">Orders</h1>
+      {products.length === 0 ? (
+        <p>No orders available</p>
+      ) : (
+        products.map((product, index) => (
+          <div key={index} className="flex flex-col border justify-center hover:shadow-lg transition-shadow duration-300 items-center gap-4 mt-4 w-[300px] p-4">
+            <h1 className="text-2xl">Order {index + 1}</h1>
+            <img src="/images/order.jfif" alt={`Order ${index + 1}`} className="w-full h-auto" />
+            {product.map((item, idx) => (
+              <div key={idx} className="flex flex-col items-center gap-2">
+                <div className="text-xl">{idx + 1}. {item.name}</div>
+                <div className="text-xl">Price: {item.price} × Quantity: {item.quantity}</div>
+                <div className="text-xl">Total: {item.price * item.quantity}</div>
+              </div>
+            ))}
+          </div>
+        ))
+      )}
+    </div>
+    :<h1 className='text-3xl text-center mt-5'>Please Sign in</h1>
       }
     </div>
   );
