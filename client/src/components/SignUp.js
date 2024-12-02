edd6d4c6b5ab19cb92bfb4e8037f3738768a2851
Neeraj <nkumawat34@gmail.com>
2024-12-02 18:29:34 +0530

    Added Loading Spinner

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -5,71 +5,85 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { toast, ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
-import Wishlist from './wishlist';
-export default function SignUp() {
-  const navigate=useNavigate()
-  const [email,setEmail]=useState("")
-  const [password,setPassword]=useState("")
-  const [name,setName]=useState("")
+import { Circles } from 'react-loader-spinner'; // Import spinner
 
-  const register=()=>{
-   
-    axios.post("https://foodlie-backend.onrender.com/api/auth/signup",{
-        "name": name,
-        "email": email,
-        "password": password,
-        "wishlist":[],
-        "cart":[],
-        "order":[]
-    }).then((data)=>{
-        navigate("/")
-        localStorage.setItem('token',data.data.token);
-        toast.success("SignUp Successful",{
-            position:"bottom-right"
-        })
+export default function SignUp() {
+  const navigate = useNavigate()
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [name, setName] = useState("")
+  const [loading, setLoading] = useState(false) // Loading state
+
+  const register = () => {
+    setLoading(true) // Start loading
+    axios.post("https://foodlie-backend.onrender.com/api/auth/signup", {
+      "name": name,
+      "email": email,
+      "password": password,
+      "wishlist": [],
+      "cart": [],
+      "order": []
+    }).then((data) => {
+      setLoading(false) // Stop loading
+      navigate("/")
+      localStorage.setItem('token', data.data.token);
+      toast.success("SignUp Successful", {
+        position: "bottom-right"
+      })
     })
-    .catch((err)=>{
+      .catch((err) => {
         console.log(err)
-        toast.error("Error in Signup",{
-            position:"bottom-right"
+        setLoading(false) // Stop loading
+        toast.error("Error in Signup", {
+          position: "bottom-right"
         })
-    })
+      })
   }
+
   return (
     <div>
-
-    <div className='flex flex-wrap'>
+      <div className='flex flex-wrap'>
         <div className='basis-[100%] md:basis-1/2'>
-            <img src={Menu}  className='lg-[1000px] w-[100%]   sm:h-[100vh] h-[40vh]'/>
+          <img src={Menu} className='lg-[1000px] w-[100%] sm:h-[100vh] h-[40vh]' />
         </div>
         <div className='flex flex-col justify-center md:mt-0 mt-5 ml-[20%] md:ml-0 border-gray-950 basis-1/2 mb-[30px]'>
-        <div className='flex justify-center'><IoMdArrowBack size={50} onClick={()=>navigate("/")}/></div>
-            <h1 className='text-3xl ml-[30%]'>Welcome to Foodlie</h1>
-            <p className='text-grey text-center'>Please login with details here</p>
-            <div className='flex  flex-col justify-center items-center mt-4'>
-            <label >Full Name</label>
-
-            <input className='border-2 border-grey-600 rounded-md w-[300px]' onChange={(e)=>setName(e.target.value)}/>
-            </div>
-            <div className='flex  flex-col justify-center items-center mt-4'>
-            <label >Email</label>
-
-            <input className='border-2 border-grey-600 rounded-md w-[300px]'onChange={(e)=>setEmail(e.target.value)}/>
-            </div>
-            <div className='flex flex-col justify-center items-center mt-4'>
+          <div className='flex justify-center'>
+            <IoMdArrowBack size={50} onClick={() => navigate("/")} />
+          </div>
+          <h1 className='text-3xl ml-[30%]'>Welcome to Foodlie</h1>
+          <p className='text-grey text-center'>Please login with details here</p>
+          <div className='flex  flex-col justify-center items-center mt-4'>
+            <label>Full Name</label>
+            <input className='border-2 border-grey-600 rounded-md w-[300px]' onChange={(e) => setName(e.target.value)} />
+          </div>
+          <div className='flex  flex-col justify-center items-center mt-4'>
+            <label>Email</label>
+            <input className='border-2 border-grey-600 rounded-md w-[300px]' onChange={(e) => setEmail(e.target.value)} />
+          </div>
+          <div className='flex flex-col justify-center items-center mt-4'>
             <label className=''>Password</label>
-
-            <input type="password" className='border-2 border-grey-600 rounded-md w-[300px]' onChange={(e)=>setPassword(e.target.value)}/>
-            </div>
-            <div>
-                <button className='float-right mr-[30%]'>Forgot Password</button>
-            </div>
-            <div className='flex justify-center mt-3'><button className='bg-red-400 p-2 rounded-md' style={{width:"300px"}} onClick={()=>register()}>Sign Up</button></div>
-            
+            <input type="password" className='border-2 border-grey-600 rounded-md w-[300px]' onChange={(e) => setPassword(e.target.value)} />
+          </div>
+          <div>
+            <button className='float-right mr-[30%]'>Forgot Password</button>
+          </div>
+          <div className='flex justify-center mt-3'>
+            <button
+              className='bg-red-400 p-2 rounded-md flex justify-center items-center'
+              style={{ width: "300px" }}
+              onClick={() => register()}
+              disabled={loading} // Disable button when loading
+            >
+              {loading ? (
+                <Circles height="30" width="30" color="#ffffff" ariaLabel="loading" />
+              ) : (
+                'Sign Up'
+              )}
+            </button>
+          </div>
         </div>
-
+      </div>
+      <ToastContainer />
     </div>
-    <ToastContainer />
-</div>
   )
 }
--- client/src/components/login.js
+++ client/src/components/login.js
@@ -7,72 +7,86 @@ import "react-toastify/dist/ReactToastify.css";
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { add_user } from './UserSlice';
-import Circle from 'react-loader-spinner'
+import { Circles } from 'react-loader-spinner'; // Import the Circles spinner
+
 export default function Login() {
 
-    const navigate=useNavigate()
-    const [email,setEmail]=useState('')
-    const [password,setPassword]=useState('')
-    const [loading,setLoading]=useState(false)
-    const dispatch=useDispatch()
+    const navigate = useNavigate()
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [loading, setLoading] = useState(false)
+    const dispatch = useDispatch()
 
-    const signin=()=>{
-
-
+    const signin = () => {
         setLoading(true)
-        axios.post("https://foodlie-backend.onrender.com/api/auth/login",{
+        axios.post("https://foodlie-backend.onrender.com/api/auth/login", {
             "email": email,
             "password": password
-        }).then((data)=>{
+        }).then((data) => {
             setLoading(false)
-            dispatch(add_user({name:"",email:email}))
+            dispatch(add_user({ name: "", email: email }))
             navigate("/")
-            localStorage.setItem('token',data.data.token);
-            localStorage.setItem('email',email)
-            toast.success("Signin Successful",{
-                position:"bottom-right"
+            localStorage.setItem('token', data.data.token);
+            localStorage.setItem('email', email)
+            toast.success("Signin Successful", {
+                position: "bottom-right"
             })
         })
-        .catch((err)=>{
-            console.log(err)
-            setLoading(false)
-            toast.error("Error in Signin",{
-                position:"bottom-right"
+            .catch((err) => {
+                console.log(err)
+                setLoading(false)
+                toast.error("Error in Signin", {
+                    position: "bottom-right"
+                })
             })
-        })
-
     }
-  return (
-    <div>
 
-        <div className='flex flex-wrap'>
-            <div className='basis-[100%] md:basis-1/2'>
-                <img src={Menu}  className='lg-[1000px] w-[100%] sm:h-[100vh]  h-[40vh]'/>
+    return (
+        <div>
+            <div className='flex flex-wrap'>
+                <div className='basis-[100%] md:basis-1/2'>
+                    <img src={Menu} className='lg-[1000px] w-[100%] sm:h-[100vh]  h-[40vh]' />
+                </div>
+                <div className='flex flex-col justify-center md:mt-0 mt-5 ml-[20%] md:ml-0 border-gray-950 basis-1/2 mb-[30px]'>
+                    <div className='flex justify-center'><IoMdArrowBack size={50} onClick={() => navigate("/")} /></div>
+                    <h1 className='text-3xl ml-[30%]'>Welcome to Foodlie</h1>
+                    <p className='text-grey text-center'>Please login with details here</p>
+                    <div className='flex  flex-col justify-center items-center mt-4'>
+                        <label >Email</label>
+
+                        <input className='border-2 border-grey-600 rounded-md w-[300px]' onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+                    <div className='flex flex-col justify-center items-center mt-4'>
+                        <label className=''>Password</label>
+
+                        <input type="password" className='border-2 border-grey-600 rounded-md w-[300px]' onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    <div>
+                        <button className='float-right mr-[30%]'>Forgot Password</button>
+                    </div>
+                    <div className='flex justify-center mt-3'>
+                        <button className='bg-red-400 p-2 rounded-md flex justify-center items-center'
+                            style={{ width: "300px" }} 
+                            onClick={() => signin()} 
+                            disabled={loading}>
+                            {loading ? (
+                                <Circles
+                                    height="30"
+                                    width="30"
+                                    color="#ffffff"
+                                    ariaLabel="loading"
+                                />
+                            ) : (
+                                'Sign In'
+                            )}
+                        </button>
+                    </div>
+                    <h1 className='text-center mt-3'>Don't have an account?<button className='font-bold ' onClick={() => navigate("/signup")} >Sign Up </button></h1>
+                </div>
+
             </div>
-            <div className='flex flex-col justify-center md:mt-0 mt-5 ml-[20%] md:ml-0 border-gray-950 basis-1/2 mb-[30px]'>
-            <div className='flex justify-center'><IoMdArrowBack size={50} onClick={()=>navigate("/")}/></div>
-                <h1 className='text-3xl ml-[30%]'>Welcome to Foodlie</h1>
-                <p className='text-grey text-center'>Please login with details here</p>
-                <div className='flex  flex-col justify-center items-center mt-4'>
-                <label >Email</label>
 
-                <input className='border-2 border-grey-600 rounded-md w-[300px]' onChange={(e)=>setEmail(e.target.value)}/>
-                </div>
-                <div className='flex flex-col justify-center items-center mt-4'>
-                <label className=''>Password</label>
-
-                <input type="password" className='border-2 border-grey-600 rounded-md w-[300px]' onChange={(e)=>setPassword(e.target.value)}/>
-                </div>
-                <div>
-                    <button className='float-right mr-[30%]'>Forgot Password</button>
-                </div>
-                <div className='flex justify-center mt-3'><button className='bg-red-400 p-2 rounded-md' style={{width:"300px"}} onClick={()=>signin()}>Sign In</button></div>
-                <h1 className='text-center mt-3'>Don't have an account?<button className='font-bold ' onClick={()=>navigate("/signup")} >Sign Up </button></h1>
-            </div>
-
+            <ToastContainer />
         </div>
-     
-        <ToastContainer />
-    </div>
-  )
+    )
 }
